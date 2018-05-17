0f6c193d6b556dd0e642eaa303a10c168c583fa4
flavis <flaviano.pxjunior@gmail.com>
2018-05-17 14:04:04 -0300

    Retirando nome do banco de dados

--- code/php/conn.php
+++ code/php/conn.php
@@ -1,5 +1,5 @@
 <?php
 	$user = "root";
 	$passwd = "";
-	try {$conn = new PDO("mysql:host=localhost;dbname=cp_database", $user, $passwd);} catch (PDOException $e) {echo $e->getMessage();}
+	try {$conn = new PDO("mysql:host=localhost;dbname=", $user, $passwd);} catch (PDOException $e) {echo $e->getMessage();}
 ?>
